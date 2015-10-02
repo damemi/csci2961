7fb1cd6f7e74b2d618cc06101bcf6159d6272398
Mike Dame <damemi@gmail.com>
2015-10-02 13:15:32 -0400

    Lab 5

---
+++ Lab5_Files/CMakeFiles/Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Tutorial.dir/tutorial.cxx.o"
+  "Tutorial.pdb"
+  "Tutorial"
+  "Tutorial.pdb"
+  "CMakeFiles/CMakeRelink.dir/Tutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
